a69b8c01ef24616ff634a4d5884b52ea41609ffe
vnribeiro <viniciuscostaa.ribeiro@outlook.com>
2024-08-02 12:08:01 -0300

    Add project files.

---
+++ EmployeeManagement.Test/EmployeeFactoryTest.cs
@@ -0,0 +1,22 @@
+using EmployeeManagement.Business;
+using EmployeeManagement.DataAccess.Entities;
+
+namespace EmployeeManagement.Test
+{
+    public class EmployeeFactoryTest
+    {
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_SalaryMustBe2500()
+        {
+            //Arrange
+            var employeeFactory = new EmployeeFactory();
+
+            //Act
+            var employee = (InternalEmployee) 
+                employeeFactory.CreateEmployee("John", "Doe");
+
+            //assert
+            Assert.Equal(2500, employee.Salary);
+        }
+    }
+}

0dc36afa7b15d4213f6e183685e64f60c5c39d45
vnribeiro <viniciuscostaa.ribeiro@outlook.com>
2024-08-05 20:09:07 -0300

    Asserting with Numeric Values, Floating Points, Arrays and Collections

--- EmployeeManagement.Test/EmployeeFactoryTest.cs
+++ EmployeeManagement.Test/EmployeeFactoryTest.cs
@@ -21,5 +21,75 @@ namespace EmployeeManagement.Test
             //assert
             Assert.Equal(2500, employee.Salary);
         }
+
+        /// <summary>
+        /// Salary must be between 2500 and 3500 for internal employees.
+        /// </summary>
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500And3500()
+        {
+            //Arrange
+            var employeeFactory = new EmployeeFactory();
+
+            //Act
+            var employee = (InternalEmployee)
+                employeeFactory.CreateEmployee("John", "Doe");
+
+            //assert
+            Assert.True(employee.Salary is >= 2500 and <= 3500, "Salary not in acceptable range");
+        }
+
+        /// <summary>
+        /// Salary must be between 2500 and 3500 for internal employees (Alternative).
+        /// </summary>
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500And3500_Alternative()
+        {
+            //Arrange
+            var employeeFactory = new EmployeeFactory();
+
+            //Act
+            var employee = (InternalEmployee)
+                employeeFactory.CreateEmployee("John", "Doe");
+
+            //assert
+            Assert.True(employee.Salary >= 2500);
+            Assert.True(employee.Salary <= 3500);
+        }
+
+        /// <summary>
+        /// Salary must be between 2500 and 3500 for internal employees (Alternative with InRange).
+        /// </summary>
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500And3500_AlternativeWithInRange()
+        {
+            //Arrange
+            var employeeFactory = new EmployeeFactory();
+
+            //Act
+            var employee = (InternalEmployee)
+                employeeFactory.CreateEmployee("John", "Doe");
+
+            //assert
+            Assert.InRange(employee.Salary, 2500, 3500);
+        }
+
+        /// <summary>
+        /// Salary must be between 2500 and 3500 for internal employees (Alternative with InRange).
+        /// </summary>
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_SalaryMustBe2500_PrecisionExample()
+        {
+            //Arrange
+            var employeeFactory = new EmployeeFactory();
+
+            //Act
+            var employee = (InternalEmployee)
+                employeeFactory.CreateEmployee("John", "Doe");
+            employee.Salary = 2500.123m;
+
+            //assert
+            Assert.Equal(2500, employee.Salary, 0);
+        }
     }
 }
---
+++ EmployeeManagement.Test/EmployeeServiceTests.cs
@@ -0,0 +1,142 @@
+﻿using EmployeeManagement.Business;
+using EmployeeManagement.Test.Services;
+
+namespace EmployeeManagement.Test
+{
+    public class EmployeeServiceTests
+    {
+        /// <summary>
+        /// Employee must have attended first obligatory course.
+        /// </summary>
+        [Fact]
+        public void CreateInternalEmployee_InternalEmployeeCreated_MustHaveAttendedFirstObligatoryCourse()
+        {
+            //Arrange
+            var employeeManagementTestDataRepository = new 
+                EmployeeManagementTestDataRepository();
+
+            var employeeService = new 
+                EmployeeService(employeeManagementTestDataRepository, new EmployeeFactory());
+
+            var obligatoryCourse = 
+                employeeManagementTestDataRepository.GetCourse(Guid.Parse("37e03ca7-c730-4351-834c-b66f280cdb01"));
+
+            //Act
+            var employee = employeeService.CreateInternalEmployee("John", "Doe");
+
+            //Assert
+            Assert.Contains(obligatoryCourse, employee.AttendedCourses);
+        }
+
+        /// <summary>
+        /// Employee must have attended first obligatory course. (with Object)
+        /// </summary>
+        [Fact]
+        public void CreateInternalEmployee_InternalEmployeeCreated_MustHaveAttendedFirstObligatoryCourse_WithObject()
+        {
+            //Arrange
+            var employeeManagementTestDataRepository = new
+                EmployeeManagementTestDataRepository();
+
+            var employeeService = new
+                EmployeeService(employeeManagementTestDataRepository, new EmployeeFactory());
+
+            var obligatoryCourse =
+                employeeManagementTestDataRepository.GetCourse(Guid.Parse("37e03ca7-c730-4351-834c-b66f280cdb01"));
+
+            //Act
+            var employee = employeeService.CreateInternalEmployee("John", "Doe");
+
+            //Assert
+            Assert.Contains(obligatoryCourse, employee.AttendedCourses);
+        }
+
+        /// <summary>
+        /// Employee must have attended first obligatory course. (with Predicate)
+        /// </summary>
+        [Fact]
+        public void CreateInternalEmployee_InternalEmployeeCreated_MustHaveAttendedFirstObligatoryCourse_WithPredicate()
+        {
+            //Arrange
+            var employeeManagementTestDataRepository = new
+                EmployeeManagementTestDataRepository();
+
+            var employeeService = new
+                EmployeeService(employeeManagementTestDataRepository, new EmployeeFactory());
+
+            //Act
+            var employee = employeeService.CreateInternalEmployee("John", "Doe");
+
+            //Assert
+            Assert.Contains(employee.AttendedCourses, 
+                course => course.Id == Guid.Parse("37e03ca7-c730-4351-834c-b66f280cdb01"));
+        }
+
+        /// <summary>
+        /// Employee must have attended Second obligatory course. (with Predicate)
+        /// </summary>
+        [Fact]
+        public void CreateInternalEmployee_InternalEmployeeCreated_MustHaveAttendedSecondObligatoryCourse_WithPredicate()
+        {
+            //Arrange
+            var employeeManagementTestDataRepository = new
+                EmployeeManagementTestDataRepository();
+
+            var employeeService = new
+                EmployeeService(employeeManagementTestDataRepository, new EmployeeFactory());
+
+            //Act
+            var employee = employeeService.CreateInternalEmployee("John", "Doe");
+
+            //Assert
+            Assert.Contains(employee.AttendedCourses,
+                course => course.Id == Guid.Parse("1fd115cf-f44c-4982-86bc-a8fe2e4ff83e"));
+        }
+
+        /// <summary>
+        /// Employee Attended Courses must match obligatory courses.
+        /// </summary>
+        [Fact]
+        public void CreateInternalEmployee_InternalEmployeeCreated_AttendedCoursesMustMatchObligatoryCourses()
+        {
+            //Arrange
+            var employeeManagementTestDataRepository = new
+                EmployeeManagementTestDataRepository();
+
+            var employeeService = new
+                EmployeeService(employeeManagementTestDataRepository, new EmployeeFactory());
+
+            var obligatoryCourses = 
+                employeeManagementTestDataRepository.GetCourses(
+                    Guid.Parse("37e03ca7-c730-4351-834c-b66f280cdb01"),
+                    Guid.Parse("1fd115cf-f44c-4982-86bc-a8fe2e4ff83e"));
+
+            //Act
+            var employee = employeeService.CreateInternalEmployee("John", "Doe");
+
+            //Assert
+            Assert.Equal(obligatoryCourses, employee.AttendedCourses);
+        }
+
+        /// <summary>
+        /// Employee Attended Courses must match obligatory courses.
+        /// </summary>
+        [Fact]
+        public void CreateInternalEmployee_InternalEmployeeCreated_AttendedCoursesMustNotBeNew()
+        {
+            //Arrange
+            var employeeManagementTestDataRepository = new
+                EmployeeManagementTestDataRepository();
+
+            var employeeService = new
+                EmployeeService(employeeManagementTestDataRepository, new EmployeeFactory());
+
+            //Act
+            var employee = employeeService.CreateInternalEmployee("John", "Doe");
+
+            //Assert
+            Assert.All(employee.AttendedCourses,
+                course => Assert.False(course.IsNew));
+        }
+    }
+}
